e4475f32d77f1830f94f2f8e1c1539220619defe
Max Omdal <mjomdal@gmail.com>
2016-08-28 16:14:13 -0500

    added comments to code

--- 2016-17/BinaryMath.java
+++
@@ -1,9 +0,0 @@
-// Max Omdal
-// Code Club 2016
-
-public class BinaryMath {
-  public static void main(String[] args) {
-    int x = 5;
-    System.out.println(Integer.toBinaryString(x));
-  }
-}
---
+++ 2016-17/Types And Identifiers/Primitives/BinaryMath.java
@@ -0,0 +1,12 @@
+// Max Omdal
+// Code Club 2016
+
+public class BinaryMath {
+  public static void main(String[] args) {
+    //initializes a variable with value 5
+    int x = 5;
+
+    //prints the variable x as a binary value
+    System.out.println(Integer.toBinaryString(x));
+  }
+}
