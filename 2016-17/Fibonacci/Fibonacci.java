a86b5a0ebd938a11e7d1e72383866db6d7cba8c3
Max Omdal <mjomdal@gmail.com>
2016-09-03 18:55:16 -0500

    Fibonacci project created

--- 2016-17/Fibonacci/Fibonacci.java
+++ 2016-17/Fibonacci/Fibonacci.java
@@ -6,20 +6,29 @@ class Fibonacci {
 
   // main function will doesn't need to be called, it runs on its own
   public static void main(String[] args) {
-    // get the fibonacci number for each number in the sequence and print it
+    // repeat for every number you print
     for (int i = 0; i < sizeOfSequence; i++) {
+      // print the fibonacci number at position i
       System.out.println(getFib(i));
     }
   }
 
   public static int getFib(int n) {
+    // the first element of the sequence is given as 0
     if (n == 0) {
       return 0;
     }
+
+    // the second element of the sequence is given as 1
     if (n == 1) {
       return 1;
     }
+
+    // if it is not a given element of the sequence, do this
     else {
+      // use recursion to cycle through every number in the sequence
+      // every time this return command runs
+      //the whole fibonacci sequence from element 1 to n are created
       return getFib(n - 1) + getFib(n - 2);
     }
   }
