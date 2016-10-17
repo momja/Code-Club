6d8b949b82fa320c1954319cc5a7572c0c7c2682
Max Omdal <mjomdal@gmail.com>
2016-10-17 10:07:41 -0500

    organization

--- ccintro/form/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Contact
-
-class ContactForm(forms.ModelForm):
-    
-    class Meta:
-        model = Contact
-        fields = ('name', 'number')
--- ccintro/form/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.contact_form, name='contact_form')
-]
