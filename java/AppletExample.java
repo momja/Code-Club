60202fa7743bb2e7e84e00caa6e705dc2327de33
Max Omdal <mjomdal@gmail.com>
2016-10-17 13:08:56 -0500

    java applet

---
+++ java/AppletExample.java
@@ -0,0 +1,20 @@
+import javax.swing.JApplet;
+import javax.swing.SwingUtilities;
+import javax.swing.JLabel;
+
+public class AppletExample extends JApplet {
+    //Called when this applet is loaded into the browser.
+    public void init() {
+        //Execute a job on the event-dispatching thread; creating this applet's GUI.
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    JLabel lbl = new JLabel("Hello World");
+                    add(lbl);
+                }
+            });
+        } catch (Exception e) {
+            System.err.println("createGUI didn't complete successfully");
+        }
+    }
+}
