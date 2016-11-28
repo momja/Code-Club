887b6bf6ecaed63979248bd5ddd118c87621aa72
Max Omdal <mjomdal@gmail.com>
2016-11-28 10:09:46 -0600

    more errors

--- 2016-17/Challenge Projects/Errors/Error.java
+++ 2016-17/Challenge Projects/Errors/Error.java
@@ -1,12 +1,16 @@
 //Max Omdal
 //Code Club 2016
 
-// Figure out why this isn't working
-// The output should be 922,303,610
+/* Figure out why this isn't working
+The output should be:
+922,303,610
+Hello Everyone!
+*/
 public class Error {
   public static void main(string[] args) {
     Int bigNumber = 3588730
-    Sring longString = "iaughkjnvqpiruhqgiukjnwfeiwuipauhfuigh";
+    Sring longString = "this is a bunch of words that make a string";
     System.out.println("bigNumber"*257);
+    System.out.println("Hello Everyone!);
 
 }
