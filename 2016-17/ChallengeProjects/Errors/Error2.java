d319a7230b1f70f7101266b0b1fbf1779a605206
Max Omdal <mjomdal@gmail.com>
2016-12-01 11:17:30 -0600

    for meeting 5

---
+++ 2016-17/ChallengeProjects/Errors/Error2.java
@@ -0,0 +1,26 @@
+// Max Omdal
+// Code Club 2016
+
+
+// The output should be:
+
+// to be or not to be
+// 597
+// code is cool!
+
+plublic static void main(String{} args) {
+  public class CodeClub {
+    double 10.0 = number; //a double is a number with decimal values (i.e. 6.4093)
+    String hamlet = "to be or not to be";
+    system.print("hamlet");
+    system.print(10x59.7);
+
+    boolean codeIsCool = false;
+    if (codeIsCool == true) {
+      System.print(code is cool!);
+    }
+    else {
+      System.print("code is not cool :(");
+    }
+  }
+}
